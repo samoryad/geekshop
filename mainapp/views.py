983c5a2dca1a81bd95d64e1ca0a18d06a5824697
Andrey Samoryadov <samoryad@mail.ru>
2020-11-29 13:17:03 +0300

    homework_03 tasks 1-7 done

--- mainapp/views.py
+++ mainapp/views.py
@@ -17,18 +17,18 @@ def main(request):
 
 def products(request, pk=None):
     print(pk)
-    links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+    # links_menu = [
+    #     {'href': 'products_all', 'name': 'все'},
+    #     {'href': 'products_home', 'name': 'дом'},
+    #     {'href': 'products_office', 'name': 'офис'},
+    #     {'href': 'products_modern', 'name': 'модерн'},
+    #     {'href': 'products_classic', 'name': 'классика'},
+    # ]
     # к заданию №6
     links_menu = ProductCategory.objects.all()
     content = {
         'title': 'Продукты',
-        # 'links_menu': links_menu
+        'links_menu': links_menu
     }
     return render(request, 'mainapp/products.html', content)
 
