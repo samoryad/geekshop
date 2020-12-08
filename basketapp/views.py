415554ad6e8d216c095373908a5149ca0877137e
Andrey Samoryadov <samoryad@mail.ru>
2020-12-08 23:18:34 +0300

    homework_06 task 1, 2 done

--- basketapp/views.py
+++ basketapp/views.py
@@ -4,7 +4,15 @@ from mainapp.models import Product
 
 
 def basket(request):
-    pass
+    title = 'корзина'
+    basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
+
+    content = {
+        'title': title,
+        'basket_items': basket_items,
+    }
+
+    return render(request, 'basketapp/basket.html', content)
 
 
 def basket_add(request, pk):
@@ -21,5 +29,8 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
-def basket_remove(request):
-    pass
+def basket_remove(request, pk):
+    basket_record = get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
