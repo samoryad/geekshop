d3d499861736c803a0ec137f92ca1b940b2215f0
Andrey Samoryadov <samoryad@mail.ru>
2020-12-09 21:57:10 +0300

    homework_06 tasks 1-4 done

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.urls import reverse
+
 from basketapp.models import Basket
 from mainapp.models import Product
 
 
+@login_required
 def basket(request):
     title = 'корзина'
     basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
@@ -15,9 +19,11 @@ def basket(request):
     return render(request, 'basketapp/basket.html', content)
 
 
+@login_required
 def basket_add(request, pk):
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:product', args=[pk]))
     product = get_object_or_404(Product, pk=pk)
-
     basket_item = Basket.objects.filter(user=request.user, product=product).first()
 
     if not basket_item:
@@ -29,6 +35,7 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, pk):
     basket_record = get_object_or_404(Basket, pk=pk)
     basket_record.delete()
