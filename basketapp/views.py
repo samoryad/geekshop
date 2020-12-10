c11dccccb4857bd1d3366584c5285dfdaf7da086
Andrey Samoryadov <samoryad@mail.ru>
2020-12-11 02:39:36 +0300

    homework_06 done (with basket_remove_ajax)

--- basketapp/views.py
+++ basketapp/views.py
@@ -46,6 +46,23 @@ def basket_remove(request, pk):
 
 
 @login_required
+def basket_remove_ajax(request, pk):
+    if request.is_ajax():
+        basket_for_delete = Basket.objects.get(pk=pk)
+        basket_for_delete.delete()
+
+        basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
+
+        content = {
+            'basket_items': basket_items
+        }
+
+        result = render_to_string('basketapp/includes/inc_basket_list.html', content)
+
+        return JsonResponse({'result': result})
+
+
+@login_required
 def basket_edit(request, pk, quantity):
     if request.is_ajax():
         quantity = int(quantity)
--- static/js/basket_scripts.js
+++ static/js/basket_scripts.js
@@ -43,4 +43,20 @@ window.onload = function () {
         event.preventDefault();
     });
 
+    // добавляем ajax-обработчик для удаления товара из корзины
+    $('.basket_list').on('click', '.button-remove', function () {
+        let pk = $(this).attr('data-pk');
+
+        if (pk) {
+            $.ajax({
+                url: "/basket/remove/ajax/" + pk + "/",
+
+                success: function (data) {
+                    $('.basket_list').html(data.result);
+                },
+            });
+
+        }
+        return;
+    });
 };
