855aa356a4215cc43138f48e291b64eb076ce1d4
Andrey Samoryadov <samoryad@mail.ru>
2020-12-20 00:15:17 +0300

    homework_02_01 done (to check CBV ProductUpdateView from homework_08)

--- Geekshop/settings.py
+++ Geekshop/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'mainapp.context_processors.basket',
             ],
         },
     },
@@ -133,8 +134,11 @@ DOMAIN_NAME = 'http://localhost:8000'
 
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = '25'
-# EMAIL_HOST_USER = 'django@geekbrains.local'
-# EMAIL_HOST_PASSWORD = 'geekshop'
+EMAIL_HOST_USER = 'django@geekbrains.local'
+EMAIL_HOST_PASSWORD = 'geekshop'
 EMAIL_USE_SSL = False
 
-EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
+# EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = 'tmp/emails/'
