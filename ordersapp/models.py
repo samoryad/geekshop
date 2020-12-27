2884b3b95e6fe91566cdd3349dc8d5ce1bf816ab
Andrey Samoryadov <samoryad@mail.ru>
2020-12-27 17:30:09 +0300

    homework_02_03 task 1-4 done

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+
 from mainapp.models import Product
 
 
@@ -42,5 +43,8 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='продукт')
     quantity = models.PositiveIntegerField(default=0, verbose_name='количество')
 
-    def get_product_cost(self):
+    @property
+    def product_cost(self):
         return self.product.price * self.quantity
+
+
