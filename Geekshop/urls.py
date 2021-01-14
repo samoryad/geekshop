ec084c534ddc743cc29ad38722f24a4b40ea90d5
Andrey Samoryadov <samoryad@mail.ru>
2021-01-14 05:08:07 +0300

    last commit before delete folder

--- Geekshop/urls.py
+++ Geekshop/urls.py
@@ -1,38 +1,24 @@
-"""geekshop URL Configuration
+from django.conf.urls import include
+from django.contrib import admin
+import mainapp.views as mainapp
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
-from mainapp import views as mainapp
+
+from django.urls import re_path
 
 urlpatterns = [
-    path('', mainapp.main, name='main'),
-    path('auth/', include('authapp.urls', namespace='auth')),
-    path('products/', include('mainapp.urls', namespace='products')),
-    path('basket/', include('basketapp.urls', namespace='basket')),
-    path('contact/', mainapp.contact, name='contact'),
-    path('', include('social_django.urls', namespace='social')),
-    # path('sub_admin/', admin.site.urls), старая админка
-    path('admin/', include('adminapp.urls', namespace='admin')),
-    path('order/', include('ordersapp.urls', namespace='order')),
-]
+    re_path(r'^$', mainapp.main, name='main'),
+    re_path(r'^products/', include('mainapp.urls', namespace='products')),
+    re_path(r'^contact/', mainapp.contact, name='contact'),
+    re_path(r'^auth/verify/google/oauth2/', include("social_django.urls", namespace="social")),
+    re_path(r'^auth/', include('authapp.urls', namespace='auth')),
+    re_path(r'^basket/', include('basketapp.urls', namespace='basket')),
 
-# обработка 404 ошибки
-# handler404 = 'mainapp.views.not_found'
+    re_path(r'^order/', include('ordersapp.urls', namespace='order')),
+
+    re_path(r'^admin/', include('adminapp.urls', namespace='admin')),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
