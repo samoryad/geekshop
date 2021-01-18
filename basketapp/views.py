580699d57852a3e3f3ae020c592795561b9f9160
Andrey Samoryadov <samoryad@mail.ru>
2021-01-19 00:10:22 +0300

    basketapp def basket updated (select_related)

--- basketapp/views.py
+++ basketapp/views.py
@@ -11,7 +11,7 @@ from mainapp.models import Product
 @login_required
 def basket(request):
     title = 'корзина'
-    basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
+    basket_items = Basket.objects.filter(user=request.user).order_by('product__category').select_related()
 
     content = {
         'title': title,
