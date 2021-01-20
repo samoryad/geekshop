626d523956636e3aee7ec8d91729f50686dd6697
Andrey Samoryadov <samoryad@mail.ru>
2021-01-20 09:41:00 +0300

    homework_02_07 updated basketapp/models.py (@cached_property)

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.db import models
+from django.utils.functional import cached_property
+
 from mainapp.models import Product
 
 # можно создать менеджер (в данном случае из-за удаления QuerySet в ordersapp\views.py
@@ -28,17 +30,21 @@ class Basket(models.Model):
     def product_cost(self):
         return self.product.price * self.quantity
 
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
+
     @property
     # метод, считающий количество товаров в корзине пользователя
     def total_quantity(self):
-        _user_basket_objects = Basket.objects.filter(user=self.user)
+        _user_basket_objects = self.get_items_cached
         _total_quantity = sum(list(map(lambda x: x.quantity, _user_basket_objects)))
         return _total_quantity
 
     @property
     # метод вычисления общей стоимости продуктов конкретного пользователя
     def total_cost(self):
-        _user_basket_objects = Basket.objects.filter(user=self.user)
+        _user_basket_objects = self.get_items_cached
         _total_cost = sum(list(map(lambda x: x.product_cost, _user_basket_objects)))
         return _total_cost
 
