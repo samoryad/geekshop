1bef2f470e6b244649d3bb2493aede9cabbf1fcc
Andrey Samoryadov <samoryad@mail.ru>
2021-01-26 00:07:54 +0300

    mainapp/tests.py TestMainappSmoke created

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.core.management import call_command
+from django.test import TestCase, Client
 
-# Create your tests here.
+from mainapp.models import ProductCategory, Product
+
+
+class TestMainappSmoke(TestCase):
+    def setUp(self):
+        test_category = ProductCategory.objects.create(name='Test')
+        Product.objects.create(
+            category=test_category,
+            name='Product test 1'
+        )
+        Product.objects.create(
+            category=test_category,
+            name='Product test 2'
+        )
+        # call_command('flush', '--noinput')
+        # call_command('loaddata', 'test_db.json')
+        self.client = Client()
+
+    def test_mainapp_urls(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+
+        # response = self.client.get('/products/category/0/')
+        # self.assertEqual(response.status_code, 200)
+        #
+        # for category in ProductCategory.objects.all():
+        #     response = self.client.get(f'/products/category/{category.pk}/')
+        #     self.assertEqual(response.status_code, 200)
+        #
+        # for product in Product.objects.all():
+        #     response = self.client.get(f'/products/product/{product.pk}/')
+        #     self.assertEqual(response.status_code, 200)
+
+    def tearDown(self):
+        call_command('sqlsequencereset', 'mainapp', 'authapp', 'ordersapp', 'basketapp')
