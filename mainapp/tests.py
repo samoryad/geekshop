8845eab1d0a32e3cc87083daae278e073af65852
Andrey Samoryadov <samoryad@mail.ru>
2021-01-26 01:30:41 +0300

    mainapp/tests.py test_products_pages

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -29,16 +29,17 @@ class TestMainappSmoke(TestCase):
         response = self.client.get('/products/')
         self.assertEqual(response.status_code, 200)
 
-        # response = self.client.get('/products/category/0/')
-        # self.assertEqual(response.status_code, 200)
-        #
-        # for category in ProductCategory.objects.all():
-        #     response = self.client.get(f'/products/category/{category.pk}/')
-        #     self.assertEqual(response.status_code, 200)
-        #
-        # for product in Product.objects.all():
-        #     response = self.client.get(f'/products/product/{product.pk}/')
-        #     self.assertEqual(response.status_code, 200)
+        response = self.client.get('/products/category/0/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_products_pages(self):
+        for category in ProductCategory.objects.all():
+            response = self.client.get(f'/products/category/{category.pk}/')
+            self.assertEqual(response.status_code, 200)
+
+        for product in Product.objects.all():
+            response = self.client.get(f'/products/product/{product.pk}/')
+            self.assertEqual(response.status_code, 200)
 
     def tearDown(self):
         call_command('sqlsequencereset', 'mainapp', 'authapp', 'ordersapp', 'basketapp')
