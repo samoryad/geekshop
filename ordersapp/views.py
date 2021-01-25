2c9a73e8cfd259286acc288aa493bbc8741101b8
Andrey Samoryadov <samoryad@mail.ru>
2021-01-25 20:55:57 +0300

    homework_02_08 basketapp/views updated (F object added)

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
+from django.db.models import F
 from django.db.models.signals import pre_save, pre_delete
 from django.dispatch import receiver
 from django.forms import inlineformset_factory
@@ -139,7 +140,7 @@ def product_quantity_update_save(sender, update_fields, instance, **kwargs):
 @receiver(pre_delete, sender=OrderItem)
 @receiver(pre_delete, sender=Basket)
 def product_quantity_update_delete(sender, instance, **kwargs):
-    instance.product.quantity += instance.quantity
+    instance.product.quantity = F('quantity') + instance.quantity
     instance.product.save()
 
 
