fc74b06a1efbc341d4f8c51f318f15595d230f56
Andrey Samoryadov <samoryad@mail.ru>
2021-01-27 00:21:26 +0300

    adminapp/urls.py UserUpdateView updated (bug fixed -> success_url = reverse_lazy('admin:users'))

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('users/create/', adminapp.UserCreateView.as_view(), name='user_create'),
     # path('users/read/', adminapp.users, name='users'),
     path('users/read/', adminapp.UserListView.as_view(), name='users'),
-    path('users/update/<int:pk>/', adminapp.user_update, name='user_update'),
-    # path('users/update/<int:pk>/', adminapp.UserUpdateView.as_view(), name='user_update'),
+    # path('users/update/<int:pk>/', adminapp.user_update, name='user_update'),
+    path('users/update/<int:pk>/', adminapp.UserUpdateView.as_view(), name='user_update'),
     # path('users/delete/<int:pk>/', adminapp.user_delete, name='user_delete'),
     path('users/delete/<int:pk>/', adminapp.UserDeleteView.as_view(), name='user_delete'),
 
