64100e1ee995f8656ced551913a6545695714894
Andrey Samoryadov <samoryad@mail.ru>
2021-01-27 00:39:54 +0300

    authapp/tests.py updated (test_user_register & test_user_wrong_register)

--- authapp/tests.py
+++ authapp/tests.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.test import TestCase
 from django.test.client import Client
 from authapp.models import ShopUser
@@ -43,12 +44,19 @@ class TestUserManagement(TestCase):
     def test_user_logout(self):
         self.client.login(username='django', password='geekbrains')
 
+        # логинимся
         response = self.client.get('/auth/login/')
         self.assertEqual(response.status_code, 200)
         self.assertFalse(response.context['user'].is_anonymous)
 
+        # разлогиниваемся
         response = self.client.get('/auth/logout/')
         self.assertEqual(response.status_code, 302)
+
+        # главная после выхода
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context['user'].is_anonymous)
 
     def test_basket_login_redirect(self):
         # без логина должен переадресовать
@@ -64,6 +72,58 @@ class TestUserManagement(TestCase):
         # self.assertEqual(list(response.context['basket']), [])
         self.assertEqual(response.request['PATH_INFO'], '/basket/')
         # self.assertIn('Ваша корзина, Пользователь', response.content.decode())
+
+    def test_user_register(self):
+        # логин без данных пользователя
+        response = self.client.get('/auth/register/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['title'], 'регистрация')
+        self.assertTrue(response.context['user'].is_anonymous)
+
+        new_user_data = {
+            'username': 'samuel',
+            'first_name': 'Сэмюэл',
+            'last_name': 'Джексон',
+            'password1': 'geekbrains',
+            'password2': 'geekbrains',
+            'email': 'sumuel@geekshop.local',
+            'age': '21'}
+
+        response = self.client.post('/auth/register/', data=new_user_data)
+        self.assertEqual(response.status_code, 302)
+
+        new_user = ShopUser.objects.get(username=new_user_data['username'])
+
+        activation_url = f"{settings.DOMAIN_NAME}/auth/verify/{new_user_data['email']}/{new_user.activation_key}/"
+
+        response = self.client.get(activation_url)
+        self.assertEqual(response.status_code, 200)
+
+        # данные нового пользователя
+        self.client.login(username=new_user_data['username'], password=new_user_data['password1'])
+
+        # логинимся
+        response = self.client.get('/auth/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['user'].is_anonymous)
+
+        # проверяем главную страницу
+        response = self.client.get('/')
+        self.assertContains(response, text=new_user_data['first_name'], status_code=200)
+
+    def test_user_wrong_register(self):
+        new_user_data = {
+            'username': 'teen',
+            'first_name': 'Мэри',
+            'last_name': 'Поппинс',
+            'password1': 'geekbrains',
+            'password2': 'geekbrains',
+            'email': 'merypoppins@geekshop.local',
+            'age': '17'}
+
+        response = self.client.post('/auth/register/', data=new_user_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, 'register_form', 'age', 'Вы слишком молоды!')
 
     def tearDown(self):
         call_command('sqlsequencereset', 'mainapp', 'authapp', 'ordersapp', 'basketapp')
