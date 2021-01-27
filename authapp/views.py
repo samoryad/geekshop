608a1f57f20c96586ece2dd762dcf35fb6ee5a03
Andrey Samoryadov <samoryad@mail.ru>
2021-01-27 13:54:46 +0300

    authapp/views.py def verify changed ( and not user.is_activation_key_expired() removed from if user.activation_key == activation_key)

--- authapp/views.py
+++ authapp/views.py
@@ -25,7 +25,7 @@ def send_verify_mail(user):
 def verify(request, email, activation_key):
     try:
         user = ShopUser.objects.get(email=email)
-        if user.activation_key == activation_key and not user.is_activation_key_expired():
+        if user.activation_key == activation_key:
             user.activation_key = ''
             user.is_active = True
             user.save()
