d50ea184b5f19dea1e707df15b56b91d61dc952a
Andrey Samoryadov <samoryad@mail.ru>
2021-01-28 10:24:20 +0300

    ordersapp/views.py updated instance.product.quantity = F('quantity') - sender.get_item(instance.pk).quantity

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -132,7 +132,7 @@ def product_quantity_update_save(sender, update_fields, instance, **kwargs):
     if update_fields is 'quantity' or 'product':
         if instance.pk:
             # instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
-            instance.product.quantity = F('quantity') - (instance.quantity - sender.get_item(instance.pk).quantity)
+            instance.product.quantity = F('quantity') - sender.get_item(instance.pk).quantity
         else:
             instance.product.quantity -= instance.quantity
         instance.product.save()
